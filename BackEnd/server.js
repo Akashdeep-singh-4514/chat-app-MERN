9d34c6b5ad350f4c0758a8e902dce1d22cf0aa02
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-06-24 17:08:44 +0530

    removes unnessary files

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,7 +6,7 @@ const messageRoutes = require("./routes/message.routes.js")
 const userRoutes = require("./routes/user.routes.js")
 
 const { server, app } = require("./socket/socket.js")
-
+// require("../FrontEnd/ChatApp/dist/index.html")
 
 // these three lines are to fetch environment variable form .env 
 // package dotenv shoul be installed
@@ -33,6 +33,7 @@ app.use("/api/users", userRoutes)
 var distDir = path.resolve(__dirname, "../frontend/chatapp/dist");
 app.use(express.static(distDir));
 
+// Serve index.html for all other routes
 app.get("*", function (req, res) {
     res.sendFile(path.join(distDir, "index.html"));
 });
