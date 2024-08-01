9de15767a7a02e734bee70d841e6a177f37bf660
Akashdeep singh <waliaakash95@gmail.com>
2024-08-01 12:40:33 +0530

    Update SocketContext.jsx

--- FrontEnd/ChatApp/src/contexts/SocketContext.jsx
+++ FrontEnd/ChatApp/src/contexts/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-mern-hoqc.onrender.com", {
+      const socket = io("https://chat-app-mern-d00k.onrender.com", {
         query: {
           userId: authUser._id,
         },
