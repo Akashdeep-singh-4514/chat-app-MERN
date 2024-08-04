c14443f669fb6476148419fd115ca25162e61787
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-08-04 12:01:30 +0530

    test frontend 19

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model.js");
 
 const getUsersForSidebar = async (req, res) => {
+    return res.status(200).json({ data: "Hello world" })
     try {
         const loggedInUserId = req.user._id;
         // removing ourself from side bar users
@@ -11,8 +12,6 @@ const getUsersForSidebar = async (req, res) => {
         console.error("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({ error });
         // res.status(500).json({ error: "Internal server error"});
-
-
     }
 };
 module.exports = getUsersForSidebar
